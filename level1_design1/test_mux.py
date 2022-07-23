15b85e1f47681728809dec6bc377f36d6af76b50
SOUHARDHYA PAUL <souhardhyapaul1999@gmail.com>
2022-07-23 14:16:03 +0000

    bug found

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,5 +6,15 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
+    sel = 12
+    inp12 = 1
+    inp13 = 0
+
+    dut.sel.value = sel
+    dut.inp12.value = inp12
+
+    await Timer(2, units='ns')
+
+    assert dut.out.value == 1, f"Adder result is incorrect: {dut.X.value} != 15"
 
     cocotb.log.info('##### CTB: Develop your test here ########')
